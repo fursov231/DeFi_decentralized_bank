aa645cc755db24b1a3e76488ab82c2e0b15cbf2b
vitaly231 <fursov231@Hotmail.com>
2021-06-29 18:10:26 +0300

    adding new features

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import { Tabs, Tab } from 'react-bootstrap'
+import {Tabs, Tab} from 'react-bootstrap'
 import dBank from '../abis/dBank.json'
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Token from '../abis/Token.json'
 import dbank from '../dbank.png';
 import Web3 from 'web3';
@@ -10,79 +10,161 @@ import './App.css';
 
 class App extends Component {
 
-  async componentWillMount() {
-    await this.loadBlockchainData(this.props.dispatch)
-  }
-
-  async loadBlockchainData(dispatch) {
-
-    //check if MetaMask exists
-
-      //assign to values to variables: web3, netId, accounts
-
-      //check if account is detected, then load balance&setStates, elsepush alert
-
-      //in try block load contracts
-
-    //if MetaMask not exists push alert
-  }
-
-  async deposit(amount) {
-    //check if this.state.dbank is ok
-      //in try block call dBank deposit();
-  }
-
-  async withdraw(e) {
-    //prevent button from default click
-    //check if this.state.dbank is ok
-    //in try block call dBank withdraw();
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      web3: 'undefined',
-      account: '',
-      token: null,
-      dbank: null,
-      balance: 0,
-      dBankAddress: null
+    async componentWillMount() {
+        await this.loadBlockchainData(this.props.dispatch)
     }
-  }
 
-  render() {
-    return (
-      <div className='text-monospace'>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-        <img src={dbank} className="App-logo" alt="logo" height="32"/>
-          <b>dBank</b>
-        </a>
-        </nav>
-        <div className="container-fluid mt-5 text-center">
-        <br></br>
-          <h1>{/*add welcome msg*/}</h1>
-          <h2>{/*add user address*/}</h2>
-          <br></br>
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-              <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                {/*add Tab deposit*/}
-                {/*add Tab withdraw*/}
-              </Tabs>
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    async loadBlockchainData(dispatch) {
+
+        //check if MetaMask exists
+        if (typeof window.ethereum !== "undefined") {
+            await window.ethereum.enable()
+            //assign to values to variables: web3, netId, accounts
+            const web3 = new Web3(window.ethereum)
+            const netId = await web3.eth.net.getId()
+            const accounts = await web3.eth.getAccounts()
+
+
+            //check if account is detected, then load balance&setStates, else push alert
+            if (typeof accounts[0] !== "undefined") {
+                //load balance
+                const balance = await web3.eth.getBalance(accounts[0])
+                this.setState({account: accounts[0], balance, web3})
+            } else {
+                window.alert("Please login with MetaMask!")
+            }
+
+            //in try block load contracts
+            try {
+                const token = new web3.eth.Contract(Token.abi, Token.networks[netId].address)
+                const dbank = new web3.eth.Contract(dBank.abi, dBank.networks[netId].address)
+                const dBankAddress = dBank.networks[netId].address
+                this.setState({token, dbank, dBankAddress})
+            } catch (e) {
+                console.log("Error: ", e)
+                window.alert("Contract not deployed to the current network")
+            }
+        } else {
+            window.alert("Please install MetaMask!")
+        }
+
+        //if MetaMask not exists push alert
+    }
+
+    async deposit(amount) {
+        //check if this.state.dbank is ok
+        if (this.state.dbank !== "undefined") {
+            //in try block call dBank deposit();
+            try {
+                await this.state.dbank.methods.deposit().send({value: amount.toString(), from: this.state.account})
+            } catch (e) {
+                console.log("Error, deposit: ", e)
+            }
+        }
+
+
+    }
+
+    async withdraw(e) {
+        //prevent button from default click
+        e.preventDefault()
+        //check if this.state.dbank is ok
+        if (this.state.dbank !== "undefined") {
+            //in try block call dBank withdraw();
+            try {
+                await this.state.dbank.methods.withdraw().send({from: this.state.account})
+            } catch (e) {
+                console.log("Error, withdraw: ", e)
+            }
+        }
+
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            web3: 'undefined',
+            account: '',
+            token: null,
+            dbank: null,
+            balance: 0,
+            dBankAddress: null
+        }
+    }
+
+    render() {
+        return (
+            <div className='text-monospace'>
+                <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+                    <a
+                        className="navbar-brand col-sm-3 col-md-2 mr-0"
+                        href="#"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={dbank} className="App-logo" alt="logo" height="32"/>
+                        <b>dBank</b>
+                    </a>
+                </nav>
+                <div className="container-fluid mt-5 text-center">
+                    <br></br>
+                    <h1>Welcome to dBank</h1>
+                    <h2>{this.state.account}</h2>
+                    <br></br>
+                    <div className="row">
+                        <main role="main" className="col-lg-12 d-flex text-center">
+                            <div className="content mr-auto ml-auto">
+                                <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                                    <Tab eventKey="deposit" title="Deposit">
+                                        <div>
+                                            <br/>
+                                            How much do you want do deposit?
+                                            <br/>
+                                            (1 deposit is possible at the time)
+                                            <br/>
+                                            <form onSubmit={(e) => {
+                                                e.preventDefault()
+                                                let amount = this.depositAmount.value
+                                                amount = amount * 10 ** 18 //convert to wei
+                                                this.deposit(amount)
+                                            }}>
+                                                <div className="form-group mr-sm-2">
+                                                    <br/>
+                                                    <input id="depositAmount"
+                                                           step="0.01"
+                                                           type="number"
+                                                           className="form-control form-control-md"
+                                                           placeholder="amount..."
+                                                           required
+                                                           ref={(input) => {
+                                                               this.depositAmount = input
+                                                           }}
+                                                    />
+                                                </div>
+                                                <button type="submit" className="btn btn-primary">DEPOSIT</button>
+                                            </form>
+                                        </div>
+                                    </Tab>
+                                    <Tab eventKey="withdraw" title="Withdraw">
+                                        <div>
+                                            <br/>
+                                            Do you want to withdraw and take interest?
+                                            <br/><br/>
+                                        </div>
+                                        <div>
+                                            <button type="submit" className="btn btn-primary"
+                                                    onClick={(e) => this.withdraw(e)}>WITHDRAW
+                                            </button>
+                                        </div>
+                                    </Tab>
+                                </Tabs>
+                            </div>
+                        </main>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
